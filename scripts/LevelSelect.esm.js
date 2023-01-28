539cb375884a522a0006574b87d63874702fe662
tomek-zawadzki <65092372+tomek-zawadzki@users.noreply.github.com>
2023-01-28 22:22:08 +0100

    create level buttons and add functionality for them

--- scripts/LevelSelect.esm.js
+++ scripts/LevelSelect.esm.js
@@ -1,10 +1,41 @@
-import { Common } from "./Common.esm.js";
+import { Common, HIDDEN_SCREEN } from "./Common.esm.js";
 
+const gameLevels = [
+  {
+    level: 1,
+  },
+  {
+    level: 2,
+  },
+  {
+    level: 3,
+  },
+];
+
+const LEVEL_SELECT_BUTTON_ID = "level-select__button";
 const LEVEL_SELECT_ID = "js-level-select-screen";
 
 class LevelSelect extends Common {
   constructor() {
     super(LEVEL_SELECT_ID);
+    gameLevels.forEach((gameLevel) => this.createButton(gameLevel.level));
+  }
+
+  createButton(value) {
+    const button = document.createElement("button");
+
+    button.type = "button";
+    button.classList.add(LEVEL_SELECT_BUTTON_ID);
+    button.textContent = value;
+    button.value = value;
+    button.addEventListener("click", (event) =>
+      this.buttonOnClickHandler(event)
+    );
+    this.element.appendChild(button);
+  }
+
+  buttonOnClickHandler(event) {
+    this.changeVisibilityScreen(this.element, HIDDEN_SCREEN);
   }
 }
 
