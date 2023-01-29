f6b7da3ccb98b47ea9fabe7ad4d8eef6fd045e39
tomek-zawadzki <65092372+tomek-zawadzki@users.noreply.github.com>
2023-01-29 16:48:13 +0100

    create class sprite

---
+++ scripts/Sprite.esm.js
@@ -0,0 +1,48 @@
+import { canvas } from "./Canvas.esm.js";
+
+export class Sprite {
+  constructor(
+    x,
+    y,
+    width,
+    height,
+    spritesImage,
+    numberOfSprites = 1,
+    offeset = { x: 0, y: 0 }
+  ) {
+    this.alpha = 255;
+    this.height = height;
+    this.numberOfSprites = numberOfSprites;
+    this.offeset = { ...offeset };
+    this.spritesImage = spritesImage;
+    this.width = width;
+    this.x = x;
+    this.y = y;
+  }
+
+  draw(numberOfSprites = 0, ratio = 1) {
+    if (numberOfSprites > this.numberOfSprites) {
+      return;
+    }
+
+    if (this.aplha !== 255) {
+      canvas.context.globalAlpha = this.alpha / 255;
+    }
+
+    canvas.context.drawImage(
+      this.spritesImage,
+      numberOfSprites * this.width,
+      0,
+      this.width,
+      this.height,
+      this.x + this.offeset.x,
+      this.y + this.offeset.y,
+      this.width * ratio,
+      this.height * this.height
+    );
+
+    if (this.alpha !== 255) {
+      canvas.context.globalAlpha = 1;
+    }
+  }
+}
